2218ac9e7435260e959fb16cd96de6f48352f9e0
Vadim Zakovriashin <zakovryashin.v@gmail.com>
2026-02-19 16:53:42 +0700

    Added clouds and music and animations

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,40 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import gsap, { Linear } from "gsap"
+// import music from "./assets/music.mp3"
+import "./style.css"
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// const audio = new Audio(music)
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const cloud1 = gsap.timeline({ repeat: -1, delay: 0 })
+cloud1
+  .to("#cloud1", {
+    duration: 6,
+    x: "-150vw",
+    ease: Linear.easeNone,
+  })
+  .call(() => {
+    // audio.play()
+  })
+
+const cloud2 = gsap.timeline({ repeat: -1, delay: 4 })
+cloud2.to("#cloud2", { duration: 10, x: "-150vw", ease: Linear.easeNone })
+
+const cloud3 = gsap.timeline({ repeat: -1, delay: -6 })
+cloud3.to("#cloud3", {
+  duration: 5,
+  x: "-150vw",
+  ease: Linear.easeNone,
+})
+
+const cloud4 = gsap.timeline({ repeat: -1, delay: -7 })
+cloud4.to("#cloud4", {
+  duration: 13,
+  x: "-150vw",
+  ease: Linear.easeNone,
+})
+
+const cloud5 = gsap.timeline({ repeat: -1, delay: 0 })
+cloud5.to("#cloud5", {
+  duration: 3,
+  x: "-150vw",
+  ease: Linear.easeNone,
+})
