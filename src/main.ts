b2bebc2b2b1a9c2de3ae3429b8aeb57878f4ebe1
Vadim Zakovriashin <zakovryashin.v@gmail.com>
2026-02-25 16:40:34 +0700

    Fixed animations

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import initClouds from "./components/clouds"
 // import initKeywords from "./components/keywords"
 // import initAiIcons from "./components/ai-icons"
-import initBars from "./components/bars"
+// import initBars from "./components/bars"
 import "./style.css"
 
 initClouds()
 // initKeywords()
 // initAiIcons()
-initBars()
+// initBars()
