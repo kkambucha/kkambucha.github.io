15cbfbcd9554a1ddeb0449f33f55a682e29a41bf
Vadim Zakovriashin <zakovryashin.v@gmail.com>
2026-02-26 13:47:17 +0700

    Added some animations

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import initClouds from "./components/clouds"
 // import initKeywords from "./components/keywords"
 // import initAiIcons from "./components/ai-icons"
 // import initBars from "./components/bars"
+import initPerson from "./components/person"
 import "./style.css"
 
 initClouds()
 // initKeywords()
 // initAiIcons()
 // initBars()
+initPerson()
